24710411a4b95ba80868853d3b7aa0b91e4708ad
Matthias-Christian Ott <ott@mirix.org>
2021-06-26 22:13:58 +0200

    Remove Text Domain header field
    It seems that if the required WordPress version is 4.6, GlotPress does
    not require the Text Domain header field anymore.

--- attachment-url-version.php
+++ attachment-url-version.php
@@ -8,7 +8,6 @@
  * Author: Matthias-Christian Ott
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: attachment-url-version
  */
 
 /* Attachment URL Version WordPress Plugin
