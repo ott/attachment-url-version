259e689e15659f6db1bb84588f1d7a18b07d4b84
Matthias-Christian Ott <ott@mirix.org>
2021-06-26 21:52:18 +0200

    Add Text Domain header field
    According to the WordPress documentation, the Text Domain header field
    is no longer required since WordPress 4.6. However, GlotPress complains
    that the plugin is not properly prepared for localization because it is
    missing the Text Domain header field.

--- attachment-url-version.php
+++ attachment-url-version.php
@@ -8,6 +8,7 @@
  * Author: Matthias-Christian Ott
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: attachment-url-version
  */
 
 /* Attachment URL Version WordPress Plugin
