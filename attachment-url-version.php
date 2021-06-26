3122842ff930df2c9eee592591cfa2b30219a2b7
Matthias-Christian Ott <ott@mirix.org>
2021-06-26 23:05:14 +0200

    Add missing empty line in license comment

--- attachment-url-version.php
+++ attachment-url-version.php
@@ -12,6 +12,7 @@
 
 /* Attachment URL Version WordPress Plugin
  * Copyright (C) 2021  Matthias-Christian Ott
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
